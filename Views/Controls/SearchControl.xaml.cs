e5724976bdf3159610b96ed0254689878d98461e
Andrey Simanov <griffy@mail.ru>
2024-04-18 02:04:54 +0300

    Added SearchControl

---
+++ Views/Controls/SearchControl.xaml.cs
@@ -0,0 +1,31 @@
+using System.Windows.Input;
+
+namespace FireEscape.Views.Controls;
+
+public partial class SearchControl : ContentView
+{
+
+    public static readonly BindableProperty SearchProperty = BindableProperty.Create(nameof(Search), typeof(string), typeof(SearchControl), defaultBindingMode: BindingMode.TwoWay);
+    public string Search
+    {
+        get => (string)GetValue(SearchProperty);
+        set => SetValue(SearchProperty, value);
+    }
+
+    public static readonly BindableProperty SearchCommandProperty = BindableProperty.Create(nameof(SearchCommand), typeof(ICommand), typeof(SearchControl));
+    public ICommand SearchCommand
+    {
+        get => (ICommand)GetValue(SearchCommandProperty);
+        set => SetValue(SearchCommandProperty, value);
+    }
+
+    public int StoppedTypingTimeThreshold { get => stoppedTyping.StoppedTypingTimeThreshold; set => stoppedTyping.StoppedTypingTimeThreshold = value; }
+    public string Placeholder { get => searchEntry.Placeholder; set => searchEntry.Placeholder = value; }
+    
+    public ICommand? SearchCommand1 { get => stoppedTyping.Command; set => stoppedTyping.Command = value; }
+
+    public SearchControl()
+    {
+        InitializeComponent();
+    }
+}
