617137960cfd56682036285de9434d6b30c27283
Andrey Simanov <griffy@mail.ru>
2024-05-30 17:53:36 +0300

    Set fenceHeight platformFenceHeight as ServiceabilityProperty

--- Models/StairsElements/FenceP2.cs
+++ Models/StairsElements/FenceP2.cs
@@ -2,12 +2,12 @@
 
 public partial class FenceP2 : BaseStairsElement
 {
+    [ObservableProperty]
+    ServiceabilityProperty<float> fenceHeight = new() { ServiceabilityType = ServiceabilityTypeEnum.Auto };
+
     public override StairsElementTypeEnum StairsElementType => StairsElementTypeEnum.FenceP2;
     public override BaseStairsTypeEnum BaseStairsType => BaseStairsTypeEnum.P2;
     public override bool Required => true;
     public override int PrintOrder => 20;
     public override int TestPointCount => 1;
-
-    [ObservableProperty]
-    float fenceHeight;
 }
--- Models/StairsElements/PlatformP1.cs
+++ Models/StairsElements/PlatformP1.cs
@@ -2,10 +2,10 @@
 
 public partial class PlatformP1 : BasePlatformElement
 {
+    [ObservableProperty]
+    ServiceabilityProperty<float> platformFenceHeight = new() { ServiceabilityType = ServiceabilityTypeEnum.Auto };
+
     public override StairsElementTypeEnum StairsElementType => StairsElementTypeEnum.PlatformP1;
     public override int PrintOrder => 40;
     public override int TestPointCount => 1;
-
-    [ObservableProperty]
-    float platformFenceHeight;
 }
