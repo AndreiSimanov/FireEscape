2740e5768815e29cf0d88320e636a87656df8fd5
Andrey Simanov <griffy@mail.ru>
2024-05-31 08:43:05 +0300

    Added StairsElementResult Name elementNumbers -> ToRangeString

--- Reports/ReportDataProviders/StairsElementResult.cs
+++ Reports/ReportDataProviders/StairsElementResult.cs
@@ -1,4 +1,6 @@
-﻿namespace FireEscape.Reports.ReportDataProviders;
+﻿using System.Text;
+
+namespace FireEscape.Reports.ReportDataProviders;
 
 public record StairsElementResult(BaseStairsElement[] StairsElements, bool IsAbsent, List<string> Summary)
 {
@@ -8,7 +10,7 @@ public record StairsElementResult(BaseStairsElement[] StairsElements, bool IsAbs
         {
             var elementNumber = string.Empty;
             if (StairsElementType == StairsElementTypeEnum.PlatformP2 || StairsElementType == StairsElementTypeEnum.StairwayP2)
-                elementNumber = GetElementNumber(StairsElements.Select(element => element.ElementNumber));
+                elementNumber = ToRangeString(StairsElements.Select(element => element.ElementNumber));
             return $"{StairsElements.First().Name} {elementNumber}";
         }
     }
@@ -18,10 +20,44 @@ public record StairsElementResult(BaseStairsElement[] StairsElements, bool IsAbs
     public float WithstandLoadCalcResult => StairsElements.First().WithstandLoadCalcResult;
     public int PrintOrder => StairsElements.First().PrintOrder;
 
-    static string GetElementNumber(IEnumerable<int> numbers)
+    static string ToRangeString(IEnumerable<int> nums)
     {
-        return string.Join(",", numbers.OrderBy(num => num));
-    }
+        var sb = new StringBuilder();
+        int distance = 0;
+        int currentNum = int.MinValue;
+        foreach (var num in nums.OrderBy(num => num))
+        {
+            if (currentNum == int.MinValue)
+            {
+                sb.Append(num);
+                currentNum = num;
+                continue;
+            }
 
+            if (currentNum == num)
+                continue;
+
+            if (currentNum == num - ++distance)
+                continue;
+
+            if (currentNum != currentNum + distance - 1)
+            {
+                sb.Append(distance == 2 ? ',' : '-');
+                sb.Append(currentNum + distance - 1);
+            }
+
+            sb.Append(',');
+            sb.Append(num);
+            currentNum = num;
+            distance = 0;
+        }
+
+        if (distance > 0)
+        {
+            sb.Append(distance == 1 ? ',' : '-');
+            sb.Append(currentNum + distance);
+        }
+        return sb.ToString();
+    }
 }
 
