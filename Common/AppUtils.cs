1d8d24782c83f73ba280519bb53ab7046b80660e
Andrey Simanov <griffy@mail.ru>
2024-09-13 02:57:03 +0300

    Fixed ToValidFileName for external storage

--- Common/AppUtils.cs
+++ Common/AppUtils.cs
@@ -1,10 +1,12 @@
 ﻿using System.Diagnostics;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace FireEscape.Common;
 
 public static class AppUtils
 {
+    const string MULTIPLE_SPACES_PATTERN = @"([ ])\1+";
 
     public static string GetDefaultContentFolder(string applicationFolderName)
     {
@@ -42,7 +44,17 @@ public static class AppUtils
         return false;
     }
 
-    public static string ToValidFileName(string fileName) => Path.GetInvalidFileNameChars().Aggregate(fileName, (f, c) => f.Replace(c, '_'));
+    public static string ToValidFileName(string fileName) =>
+         Regex.Replace(GetInvalidFileNameChars().Aggregate(fileName, (f, c) => f.Replace(c, ' ')), MULTIPLE_SPACES_PATTERN, " ").Trim();
+
+    public static char[] GetInvalidFileNameChars() =>
+        [
+            '\"', '<', '>', '|', '\0',
+            (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+            (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+            (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+            (char)31, ':', '*', '?', '\\', '/'
+        ];
 
     public static string CreateFolderIfNotExists(string path, string folderName = "")
     {
